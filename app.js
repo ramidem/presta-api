a8369673e564e1786426e103244b88223bb664ac
Med Aduh <im.ramidem@gmail.com>
2020-09-11 01:04:37 +0800

    clean slate

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use("/", (req, res) => {
-  res.send({
-    message: "hello world!",
-  });
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
 app.listen(port, () => {
