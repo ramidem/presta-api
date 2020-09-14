559d972980639e463bf20023111271fbe4b0ac7d
Med Aduh <im.ramidem@gmail.com>
2020-09-15 02:55:01 +0800

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 // parse incoming requests to json format
 // this will create body attribute to req object
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello");
